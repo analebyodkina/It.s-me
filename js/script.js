a856dd71ebe781ab195983932fc0e39d69a5779f
analebyodkina <dp030992cad@gmail.com>
2023-05-25 14:35:31 +0300

    added modal window function

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ function navToggle() {
 
 // accordion
 const accordionItems = document.querySelectorAll('.accordion__item')
-
 accordionItems.forEach((item) =>{
     const accordionHeader = item.querySelector('.accordion__header')
     
@@ -20,7 +19,6 @@ accordionItems.forEach((item) =>{
         }
     })
 })
-
 const toggleItem = (item) =>{    
     const accordionContent = item.querySelector('.accordion__content')
     
@@ -62,3 +60,42 @@ $('.owl-carousel').owlCarousel({
 // copyright
 var currentYear = new Date().getFullYear();
 document.getElementById("footer-year").innerHTML = currentYear;
+
+// modal window
+const popUp = document.getElementById('popUp');
+function popup(){
+      document.getElementById('headerUl').classList.remove("active");
+      document.getElementById("html").classList.add("overflow-hidden");
+      document.getElementById("body").classList.add("overflow-hidden");
+      // Remove the class "is-active" from the burger button
+      $(".header__burger").removeClass("is-active"); 
+      popUp.classList.add('active');
+      return false;
+}
+
+
+// modal window to close by click on black bg
+popUp.addEventListener('click', function(){
+    popUp.classList.remove('active');
+    document.getElementById("html").classList.remove("overflow-hidden");
+    document.getElementById("body").classList.remove("overflow-hidden");
+    var titleElements = document.querySelectorAll('.field__title');
+    titleElements.forEach(function(titleElement) {
+    titleElement.classList.remove('active');
+  });    
+});
+const closePopUp = document.getElementById('closePopUp');
+closePopUp.addEventListener('click', () => {
+    popUp.classList.remove('active');
+    document.getElementById("html").classList.remove("overflow-hidden");
+    document.getElementById("body").classList.remove("overflow-hidden");
+    var titleElements = document.querySelectorAll('.field__title');
+    titleElements.forEach(function(titleElement) {
+    titleElement.classList.remove('active');
+  });
+        
+});
+
+popUp.querySelector('.pop-up__body').addEventListener('click', function(event){  
+  event.stopPropagation();})
+
